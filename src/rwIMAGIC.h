a82454eb8a7887672589fce9d807ea97e4c0723b
James Hooker <jhooker@mrc-lmb.cam.ac.uk>
2022-03-04 14:16:40 +0000

    Using new `MetaDataTable::getValue`.

--- src/rwIMAGIC.h
+++ src/rwIMAGIC.h
@@ -38,8 +38,8 @@
 /** Imagic Header
   * @ingroup Imagic
 */
-struct IMAGIChead
-{             // file header for IMAGIC data
+struct IMAGIChead {             
+    // file header for IMAGIC data
     int imn;          //  0      image location number (1,2,...)
     int ifn;          //  1      # images following
     int ierror;       //  2      error code: error if >0
@@ -105,25 +105,23 @@ struct IMAGIChead
 /** Imagic reader
   * @ingroup Imagic
 */
-int  readIMAGIC(long int img_select)
-{
-#ifdef DEBUG
+int  readIMAGIC(long int img_select) {
+    #ifdef DEBUG
     printf("DEBUG readIMAGIC: Reading Imagic file\n");
-#endif
+    #endif
 
     IMAGIChead* header = new IMAGIChead;
 
-    if ( fread( header, IMAGICSIZE, 1, fhed ) < 1 )
+    if (fread(header, IMAGICSIZE, 1, fhed) < 1)
         REPORT_ERROR((std::string)"readIMAGIC: header file of " + filename + " cannot be read");
 
     // Determine byte order and swap bytes if from little-endian machine
-    char*   b = (char *) header;
+    char* b = (char *) header;
     long int extent = IMAGICSIZE - 916;  // exclude char bytes from swapping
-    if ( ( abs(header->nyear) > SWAPTRIG ) || ( header->ixlp > SWAPTRIG ) )
-    {
-        for ( i=0; i<extent; i+=4 )
-            if ( i != 56 )          // exclude type string
-                swapbytes(b+i, 4);
+    if ((abs(header->nyear) > SWAPTRIG) || (header->ixlp > SWAPTRIG)) {
+        for (i = 0; i < extent; i += 4)
+            if (i != 56)          // exclude type string
+                swapbytes(b + i, 4);
     }
     long int _xDim,_yDim,_zDim;
     long int _nDim;
@@ -134,73 +132,60 @@ int  readIMAGIC(long int img_select)
 
     std::stringstream Num;
     std::stringstream Num2;
-    if ( img_select > (long int)_nDim )
-    {
+    if (img_select > (long int) _nDim) {
         Num  << img_select;
         Num2 << _nDim;
-        REPORT_ERROR((std::string)"readImagic: Image number " + Num.str() +
-                     " exceeds stack size " + Num2.str());
+        REPORT_ERROR((std::string)"readImagic: Image number " + Num.str() + " exceeds stack size " + Num2.str());
     }
 
-    if( img_select > -1)
-        _nDim=1;
-    data.setDimensions( //setDimensions do not allocate data
-        _xDim,
-        _yDim,
-        _zDim,
-        _nDim );
-    replaceNsize=_nDim;
-    DataType datatype;
+    if (img_select > -1)
+        _nDim = 1;
+    // setDimensions do not allocate data
+    data.setDimensions(_xDim, _yDim, _zDim, _nDim);
+    replaceNsize = _nDim;
 
-    if ( strstr(header->type,"PACK") )
+    DataType datatype;
+    if (strstr(header->type, "PACK")) {
         datatype = UChar;
-    else if ( strstr(header->type,"INTG") )
+    } else if (strstr(header->type, "INTG")) {
         datatype = Short;
-    else if ( strstr(header->type,"REAL") )
+    } else if (strstr(header->type, "REAL")) {
         datatype = Float;
-    else if ( strstr(header->type,"RECO") || strstr(header->type,"COMP") )
-    {
+    } else if (strstr(header->type, "RECO") || strstr(header->type,"COMP")) {
         REPORT_ERROR("readIMAGIC: only real-space images can be read into RELION");
     }
 
     // Set min-max values and other statistical values
-    if ( header->sigma == 0 && header->varian != 0 )
+    if (header->sigma == 0 && header->varian != 0) {
         header->sigma = sqrt(header->varian);
-    if ( header->densmax == 0 && header->densmin == 0 && header->sigma != 0 )
-    {
+    }
+    if (header->densmax == 0 && header->densmin == 0 && header->sigma != 0) {
         header->densmin = header->avdens - header->sigma;
         header->densmax = header->avdens + header->sigma;
     }
 
-    MDMainHeader.setValue(EMDL_IMAGE_STATS_MIN,(RFLOAT)header->densmin);
-    MDMainHeader.setValue(EMDL_IMAGE_STATS_MAX,(RFLOAT)header->densmax);
-    MDMainHeader.setValue(EMDL_IMAGE_STATS_AVG,(RFLOAT)header->avdens);
-    MDMainHeader.setValue(EMDL_IMAGE_STATS_STDDEV,(RFLOAT)header->sigma);
-    setSamplingRateInHeader((RFLOAT)1.);
-    MDMainHeader.setValue(EMDL_IMAGE_DATATYPE,(int)datatype);
+    MDMainHeader.setValue(EMDL_IMAGE_STATS_MIN, (RFLOAT) header->densmin);
+    MDMainHeader.setValue(EMDL_IMAGE_STATS_MAX, (RFLOAT) header->densmax);
+    MDMainHeader.setValue(EMDL_IMAGE_STATS_AVG, (RFLOAT) header->avdens);
+    MDMainHeader.setValue(EMDL_IMAGE_STATS_STDDEV, (RFLOAT) header->sigma);
+    setSamplingRateInHeader((RFLOAT) 1.0);
+    MDMainHeader.setValue(EMDL_IMAGE_DATATYPE, (int) datatype);
 
     offset = 0;   // separate header file
 
-    if (dataflag<0)   // Don't read the individual header and the data if not necessary
-    {
+    if (dataflag < 0) {
+        // Don't read the individual header and the data if not necessary
     	delete header;
     	return 0;
     }
 
    // Get the header information
-    int error_fseek;
-    if ( img_select > -1 )
-    	error_fseek = fseek( fhed, img_select * IMAGICSIZE, SEEK_SET );
-    else
-    	error_fseek = fseek( fhed, 0, SEEK_SET );
-
-    if (error_fseek != 0)
-    	return -1;
+    int error_fseek = img_select > -1 ? fseek(fhed, img_select * IMAGICSIZE, SEEK_SET) : fseek(fhed, 0, SEEK_SET);
+    if (error_fseek != 0) return -1;
 
     delete header;
 
-    int pad=0;
-    return readData(fimg, img_select, datatype, pad );
+    return readData(fimg, img_select, datatype, 0);
 
 }
 
@@ -218,8 +203,7 @@ int  readIMAGIC(long int img_select)
 /** Imagic Writer
   * @ingroup Imagic
 */
-void  writeIMAGIC(long int img_select=-1, int mode=WRITE_OVERWRITE)
-{
+void  writeIMAGIC(long int img_select=-1, int mode=WRITE_OVERWRITE) {
     //    if ( p->transform != NoTransform )
     //        img_convert_fourier(p, Centered);
 
@@ -238,7 +222,7 @@ void  writeIMAGIC(long int img_select=-1, int mode=WRITE_OVERWRITE)
     header->ifn = Ndim - 1 ;
 
     time_t timer;
-    time ( &timer );
+    time (&timer);
     tm* t = localtime(&timer);
 
     header->ndate = t->tm_mday;
@@ -249,35 +233,40 @@ void  writeIMAGIC(long int img_select=-1, int mode=WRITE_OVERWRITE)
     header->nsec = t->tm_sec;
 
     // Convert T to datatype
-    if ( typeid(T) == typeid(RFLOAT) ||
-         typeid(T) == typeid(float) ||
-         typeid(T) == typeid(int) )
-        strncpy(header->type,"REAL",4);
-    else if ( typeid(T) == typeid(unsigned char) ||
-              typeid(T) == typeid(signed char) )
-        strncpy(header->type,"PACK",4);
-    else
+    if (
+        typeid(T) == typeid(RFLOAT) ||
+        typeid(T) == typeid(float) ||
+        typeid(T) == typeid(int)
+    ) {
+        strncpy(header->type, "REAL", 4);
+    } else if (
+        typeid(T) == typeid(unsigned char) ||
+        typeid(T) == typeid(signed char)
+    ) {
+        strncpy(header->type, "PACK", 4);
+    } else {
         REPORT_ERROR("ERROR write IMAGIC image: invalid typeid(T)");
+    }
 
     size_t datasize, datasize_n;
-    datasize_n = Xdim*Ydim*Zdim;
+    datasize_n = Xdim * Ydim * Zdim;
     datasize = datasize_n * gettypesize(Float);
-    RFLOAT aux;
 
-    if (!MDMainHeader.isEmpty())
-    {
-
-        if(MDMainHeader.getValue(EMDL_IMAGE_STATS_MIN,   aux))
-            header->densmin = (float)aux;
-        if(MDMainHeader.getValue(EMDL_IMAGE_STATS_MAX,   aux))
-            header->densmax = (float)aux;
-        if(MDMainHeader.getValue(EMDL_IMAGE_STATS_AVG,   aux))
-            header->avdens   = (float)aux;
-        if(MDMainHeader.getValue(EMDL_IMAGE_STATS_STDDEV,aux))
-        {
-            header->sigma  = (float)aux;
-            header->varian = (float)(aux*aux);
-        }
+    if (!MDMainHeader.isEmpty()) {
+        try {
+            header->densmin = (float) MDMainHeader.getValue(EMDL_IMAGE_STATS_MIN);
+        } catch (const char* errmsg) {}
+        try {
+            header->densmax = (float) MDMainHeader.getValue(EMDL_IMAGE_STATS_MAX);
+        } catch (const char* errmsg) {}
+        try {
+            header->avdens = (float) MDMainHeader.getValue(EMDL_IMAGE_STATS_AVG);
+        } catch (const char* errmsg) {}
+        try {
+            float sigma = (float) MDMainHeader.getValue(EMDL_IMAGE_STATS_STDDEV);
+            header->sigma = sigma;
+            header->varian = sigma * sigma;
+        } catch (const char* errmsg) {}
     }
 
     memcpy(header->lastpr, "Xmipp", 5);
@@ -293,28 +282,22 @@ void  writeIMAGIC(long int img_select=-1, int mode=WRITE_OVERWRITE)
     fl.l_start  = 0;        /* Offset from l_whence         */
     fl.l_len    = 0;        /* length, 0 = to EOF           */
     fl.l_pid    = getpid(); /* our PID                      */
-    fcntl(fileno(fimg),       F_SETLKW, &fl); /* locked */
+    fcntl(fileno(fimg), F_SETLKW, &fl); /* locked */
     fcntl(fileno(fhed), F_SETLKW, &fl); /* locked */
 
+    if (mode == WRITE_APPEND) {
+        fseek(fimg, 0, SEEK_END);
+        fseek(fhed, 0, SEEK_END);
+    } else if (mode == WRITE_REPLACE) {
+        fseek(fimg, datasize   * img_select, SEEK_SET);
+        fseek(fhed, IMAGICSIZE * img_select, SEEK_SET);
+    } else /* if (mode == WRITE_OVERWRITE) */ {
+        fseek(fimg, 0, SEEK_SET);
+        fseek(fhed, 0, SEEK_SET);
+    }
+    char* fdata = (char*) askMemory(datasize);
 
-    if(mode==WRITE_APPEND)
-    {
-        fseek( fimg, 0, SEEK_END);
-        fseek( fhed, 0, SEEK_END);
-    }
-    else if(mode==WRITE_REPLACE)
-    {
-        fseek( fimg, datasize   * img_select, SEEK_SET);
-        fseek( fhed, IMAGICSIZE * img_select, SEEK_SET);
-    }
-    else //mode==WRITE_OVERWRITE
-    {
-        fseek( fimg, 0, SEEK_SET);
-        fseek( fhed, 0, SEEK_SET);
-    }
-    char* fdata = (char *) askMemory(datasize);
-
-    //Unlock
+    // Unlock
     fl.l_type   = F_UNLCK;
     fcntl(fileno(fimg), F_SETLK, &fl); /* unlocked */
     fcntl(fileno(fhed), F_SETLK, &fl); /* unlocked */
@@ -322,7 +305,6 @@ void  writeIMAGIC(long int img_select=-1, int mode=WRITE_OVERWRITE)
     freeMemory(fdata, datasize);
 
     delete header;
-
 }
 
 #endif /* RWIMAGIC_H_ */
